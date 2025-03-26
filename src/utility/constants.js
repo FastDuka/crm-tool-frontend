0524df014ff30dd3384f9182a8c90e9a524223a1
samuel kyama <samuelkyama102@gmail.com>
2025-03-26 10:16:23 +0300

    fix: standardize string formatting and update base URL comment in constants

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,5 +1,6 @@
-export const baseUrl = "https://api.fastduka.co.ke/api/"
+export const baseUrl = "https://api.fastduka.co.ke/api/";
+// export const baseUrl = "http://localhost:8000/api/";
 export const headers = {
-    "Content-Type": "application/json",
+  "Content-Type": "application/json",
 };
-const appName = 'FAST DUKA'
+const appName = "FAST DUKA";
